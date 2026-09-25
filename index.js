8a6afa1ea36401a392a48b098097272f26832af4
Duality <332583738+Duality-TOP@users.noreply.github.com>
2026-09-25 11:48:40 -0300

    backup commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function getComputerChoice() {
+    const computerSelection = Math.floor(Math.random() * 3);
+
+    switch (computerSelection) {
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
+    }
+
+    console.log(computerSelection);
+}
+
+function getHumanChoice() {
+    const humanSelection = prompt("Choose rock, paper or scissors.");
+
+    switch (humanSelection.toLowerCase()) {
+        case "rock": return "rock";
+        case "paper": return "paper";
+        case "scissors": return "scissors";
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+}
